94ff6a762e9f787d6b12dfb5c90924f10d46733d
Kim Son <kimson2111@gmail.com>
2018-04-09 13:56:02 +0700

    commit

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
