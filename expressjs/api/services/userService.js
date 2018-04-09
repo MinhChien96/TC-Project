4384c3a6cbdac34d220a01c8b78375f05c3ef902
Minh Chien <nguyenminhchien8496@gmail.com>
2018-04-09 16:03:50 +0700

    Create Struct Nodejs

---
+++ expressjs/api/services/userService.js
@@ -0,0 +1,6 @@
+const db = require("../entities");
+
+
+exports.create = function(user,res){
+
+} 
---
+++ expressjs/config/routes.js
@@ -0,0 +1,16 @@
+const userController = require('../api/controllers/userController');
+const siteController = require('../api/controllers/homePage');
+
+exports.initRoutes = function(app,express){
+    app.get('/', siteController.index);
+
+    const apiRoutes = express.Router();
+
+    var userRoutes = express.Router();
+    apiRoutes.use('/user',userRoutes);
+    userRoutes.post('/create',userController.create);
+
+
+    // Set url for API group routes
+    app.use('/api',apiRoutes);
+}
