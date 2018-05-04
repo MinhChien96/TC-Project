498a6f7ca93db69fce8db022fead56377a4bb89f
Minh Chien <nguyenminhchien8496@gmail.com>
2018-05-04 16:52:24 +0700

    validate form

--- angular/src/app/admin/add-interviewee/add-interviewee.component.ts
+++ angular/src/app/admin/add-interviewee/add-interviewee.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-import {IntervieweeService} from '../../service/interviewee.service';
+import { IntervieweeService } from '../../service/interviewee.service';
 import { Router } from '@angular/router';
 
 
@@ -12,8 +12,8 @@ import { Router } from '@angular/router';
 })
 export class AddIntervieweeComponent implements OnInit {
   filesToUpload: Array<File> = [];
-  constructor(private http: Http,private intervieweeService:IntervieweeService,private routes:Router) {}
-  user = {name:"",cv:""};
+  constructor(private http: Http, private intervieweeService: IntervieweeService, private routes: Router) { }
+  user = { name: "", cv: "" };
   ngOnInit() {
   }
   pathPDF: string;
@@ -28,18 +28,18 @@ export class AddIntervieweeComponent implements OnInit {
 
     try {
       var d = new Date();
-      var n = d.getDate().toString()+d.getMonth()+d.getFullYear();
-      var temp = this.user.name.replace(/ /g,"");
+      var n = d.getDate().toString() + (d.getMonth() + 1) + d.getFullYear();
+      var temp = this.user.name.replace(/ /g, "");
       temp = this.change_alias(temp);
-      this.user.cv =temp +"_"+n+".pdf";
+      this.user.cv = temp + "_" + n + ".pdf";
       let resCreate = await this.intervieweeService.createInter(this.user);
-      if(!resCreate.success) throw new Error("Can not insert interviewee");
-      formData.append("uploads[]", files[0],this.user.cv);
+      if (!resCreate.success) throw new Error("Can not insert interviewee");
+      formData.append("uploads[]", files[0], this.user.cv);
       let resUpload = await this.intervieweeService.upLoadCv(formData);
-      if(!resUpload.success) throw new Error("Can not upload CV");
-      this.routes.navigate(['/admin/resultquiz',resCreate.data]);
+      if (!resUpload.success) throw new Error("Can not upload CV");
+      this.routes.navigate(['/admin/resultquiz', resCreate.data]);
     } catch (error) {
-      console.log(error+"");
+      console.log(error + "");
       alert("Can not insert interviewee");
     }
   }
@@ -61,16 +61,24 @@ export class AddIntervieweeComponent implements OnInit {
   change_alias(alias) {
     var str = alias;
     str = str.toLowerCase();
-    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g,"a"); 
-    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g,"e"); 
-    str = str.replace(/ì|í|ị|ỉ|ĩ/g,"i"); 
-    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g,"o"); 
-    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g,"u"); 
-    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g,"y"); 
-    str = str.replace(/đ/g,"d");
-    str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g," ");
-    str = str.replace(/ + /g," ");
-    str = str.trim(); 
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+    str = str.replace(/đ/g, "d");
+    str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g, " ");
+    str = str.replace(/ + /g, " ");
+    str = str.trim();
     return str;
-}
+  }
+
+  validateDate(date){
+    if(Date.parse(date.value)){
+      let dt = new Date(date.value).getTime();
+      return dt>Date.now();
+    }
+    return false;
+  }
 }
